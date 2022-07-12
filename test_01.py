5fc9e1d274389c7e5c4366866dc717f3781bed1d
jugosu <whalscjf1375@gmail.com>
2022-07-12 20:12:49 +0900

    파이썬 실습

--- test_01.py
+++ test_01.py
@@ -1,2 +1,108 @@
-a,b=input().split('-')
-print(a,b,sep='')
+
+# num = int(input())
+# if num % 2 == 0: 
+#   print('짝수입니다.')
+
+ #else :
+  # print('홀수입니다.')
+
+#dust = int(input())
+
+#f dust >150:
+    #if dust>300:
+        #print('미세먼지 농도가 높습니다.')
+    #print('매우 나쁨')
+    
+#elif dust > 80:
+    #print('나쁨')
+#elif dust > 30:
+ #   print('보통')
+#else :
+ #   if dust < 0:
+  #      print('음수입니다.')
+   # else :
+    #    print('좋음')
+#n=0
+#r=0
+#user_input =int(input())
+#while n<= user_input:
+ #   print(f'n:{n}, result:{r}')
+  #  r+=n
+   # n+=1
+
+#n=0
+#r=0
+#user_input =int(input())
+#while n< user_input:
+ #   print(f'n:{n}, result:{r}')
+ #   n+=1
+ #   r+=n
+#n=int(input())
+#user_input=0
+#if user_input == n%2 and n %3 == 0:
+#    print('참') 
+#else :
+#    print('거짓')
+
+#word=input()
+#a=0
+#for b in word:
+#    a+=1
+#print(a)
+
+#word=input()
+#a=0
+#for b in range(len(word)):
+ #   a+=1
+#print(a)
+
+#a=0
+#b=0
+#num= int(input())
+#while a<num:
+ #   a+=1
+  #  b+=a
+#print(b)
+
+#a=0
+#num= int(input())
+#for b in range(1,num+1):
+#    a+=b
+#print(a)
+
+#a=0
+#b=1
+#num= int(input())
+#while a<num:
+#    a+=1
+#    b*=a
+#print(b)
+
+#=1
+#num= int(input())
+#for b in range(1,num+1):
+ #   a*=b
+#print(a)
+
+#a=[3,10,20]
+#b=(a[0]+a[1]+a[2])/3
+#print(int (b))
+
+#numbers = [0, 20, 100]
+#max = numbers[0]
+#for i in numbers:
+#    if max < i:
+#       max = i
+
+#numbers.remove(max)
+#max = numbers[0]
+#for i in numbers:
+#    if max < i:
+#       max = i
+#print(max)
+#numbers=[3,10,20]
+#sum =0
+#for i in range(3):
+#    sum+=numbers[i]
+#print(int(sum/3))
+
